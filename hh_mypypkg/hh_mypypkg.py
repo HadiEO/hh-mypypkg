1307144a9da6dffb1c1d85d460ef004e239b54aa
Hadi <hadicu06@gmail.com>
2025-07-19 22:29:07 +0200

    Add raster function (#3)

--- hh_mypypkg/hh_mypypkg.py
+++ hh_mypypkg/hh_mypypkg.py
@@ -8,3 +8,14 @@ class Map(ipyleaflet.Map):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_control(ipyleaflet.LayersControl())
+
+    def add_raster(self, raster_path):
+        """Add a raster to the map."""
+        raster = ipyleaflet.TileLayer(
+            url=raster_path,
+            name=raster_path,
+            attribution="",
+        )
+        self.add_layer(raster)
+
+        
